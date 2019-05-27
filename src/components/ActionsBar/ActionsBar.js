76aa1c6b634f0650491a3e933da908b8d91489d6
Mark Bonnici <mark.bonnici@leovegas.com>
2019-05-27 22:16:47 +0200

    remove search

--- src/components/ActionsBar/ActionsBar.js
+++ src/components/ActionsBar/ActionsBar.js
@@ -1,128 +1,134 @@
-import React from "react";
-import PropTypes from "prop-types";
-import injectSheet from "react-jss";
-import IconButton from "@material-ui/core/IconButton";
+import React from 'react'
+import PropTypes from 'prop-types'
+import injectSheet from 'react-jss'
+import IconButton from '@material-ui/core/IconButton'
 
-import Link from "gatsby-link";
-import { connect } from "react-redux";
-import screenfull from "screenfull";
+import Link from 'gatsby-link'
+import { connect } from 'react-redux'
+import screenfull from 'screenfull'
 
-import HomeIcon from "@material-ui/icons/Home";
-import SearchIcon from "@material-ui/icons/Search";
-import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
-import FullscreenIcon from "@material-ui/icons/Fullscreen";
-import FullscreenExitIcon from "@material-ui/icons/FullscreenExit";
+import HomeIcon from '@material-ui/icons/Home'
+import SearchIcon from '@material-ui/icons/Search'
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
+import FullscreenIcon from '@material-ui/icons/Fullscreen'
+import FullscreenExitIcon from '@material-ui/icons/FullscreenExit'
 
 import {
   setNavigatorPosition,
   setNavigatorShape,
   setScrollToTop,
   setFontSizeIncrease,
-  setCategoryFilter
-} from "../../state/store";
-import { featureNavigator, moveNavigatorAside } from "./../../utils/shared";
-import FontSetter from "./FontSetter";
-import CategoryFilter from "./CategoryFilter";
+  setCategoryFilter,
+} from '../../state/store'
+import { featureNavigator, moveNavigatorAside } from './../../utils/shared'
+import FontSetter from './FontSetter'
+import CategoryFilter from './CategoryFilter'
 
 const styles = theme => ({
   actionsBar: {
-    position: "absolute",
+    position: 'absolute',
     background: theme.bars.colors.background,
     left: 0,
     //top: `calc(100vh - ${theme.bars.sizes.actionsBar}px)`,
     bottom: 0,
-    display: "flex",
-    flexDirection: "row",
+    display: 'flex',
+    flexDirection: 'row',
     padding: `0 ${theme.base.sizes.linesMargin}`,
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
     height: `${theme.bars.sizes.actionsBar}px`,
-    width: "100%",
-    "&::before": {
+    width: '100%',
+    '&::before': {
       content: `""`,
-      position: "absolute",
+      position: 'absolute',
       left: theme.base.sizes.linesMargin,
       right: theme.base.sizes.linesMargin,
       height: 0,
       top: 0,
-      borderTop: `1px solid ${theme.base.colors.lines}`
+      borderTop: `1px solid ${theme.base.colors.lines}`,
     },
     [`@media (min-width: ${theme.mediaQueryTresholds.M}px)`]: {
-      padding: `0 calc(${theme.base.sizes.linesMargin} * 1.5)`
+      padding: `0 calc(${theme.base.sizes.linesMargin} * 1.5)`,
     },
     [`@media (min-width: ${theme.mediaQueryTresholds.L}px)`]: {
-      flexDirection: "column",
+      flexDirection: 'column',
       top: 0,
       right: 0,
-      left: "auto",
-      height: "100%",
+      left: 'auto',
+      height: '100%',
       padding: `${theme.base.sizes.linesMargin} 0`,
       width: `${theme.bars.sizes.actionsBar}px`,
-      "&::before": {
+      '&::before': {
         top: theme.base.sizes.linesMargin,
         bottom: theme.base.sizes.linesMargin,
         left: 0,
-        right: "auto",
+        right: 'auto',
         width: 0,
-        height: "auto",
-        borderLeft: `1px solid ${theme.base.colors.lines}`
-      }
-    }
+        height: 'auto',
+        borderLeft: `1px solid ${theme.base.colors.lines}`,
+      },
+    },
   },
   group: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
     [`@media (min-width: ${theme.mediaQueryTresholds.L}px)`]: {
-      flexDirection: "column"
-    }
+      flexDirection: 'column',
+    },
   },
   button: {
-    color: theme.bars.colors.icon
-  }
-});
+    color: theme.bars.colors.icon,
+  },
+})
 
 class ActionsBar extends React.Component {
   state = {
-    fullscreen: false
-  };
+    fullscreen: false,
+  }
 
   componentDidMount() {
     if (screenfull.enabled) {
-      screenfull.on("change", () => {
+      screenfull.on('change', () => {
         this.setState({
-          fullscreen: screenfull.isFullscreen
-        });
-      });
+          fullscreen: screenfull.isFullscreen,
+        })
+      })
     }
   }
 
-  homeOnClick = featureNavigator.bind(this);
-  searchOnClick = moveNavigatorAside.bind(this);
+  homeOnClick = featureNavigator.bind(this)
+  searchOnClick = moveNavigatorAside.bind(this)
 
   fullscreenOnClick = () => {
     if (screenfull.enabled) {
-      screenfull.toggle();
+      screenfull.toggle()
     }
-  };
+  }
 
   arrowUpOnClick = () => {
-    this.props.setScrollToTop(true);
-  };
+    this.props.setScrollToTop(true)
+  }
 
   fontSetterOnClick = val => {
-    this.props.setFontSizeIncrease(val);
+    this.props.setFontSizeIncrease(val)
 
-    if (typeof localStorage !== "undefined") {
-      localStorage.setItem("font-size-increase", val);
+    if (typeof localStorage !== 'undefined') {
+      localStorage.setItem('font-size-increase', val)
     }
-  };
+  }
 
   categoryFilterOnClick = val => {
-    this.props.setCategoryFilter(val);
-  };
+    this.props.setCategoryFilter(val)
+  }
 
   render() {
-    const { classes, navigatorPosition, navigatorShape, isWideScreen, categories } = this.props;
+    const {
+      classes,
+      navigatorPosition,
+      navigatorShape,
+      isWideScreen,
+      categories,
+    } = this.props
 
     return (
       <div className={classes.actionsBar}>
@@ -135,8 +141,12 @@ class ActionsBar extends React.Component {
           >
             <HomeIcon />
           </IconButton>
-          {((isWideScreen && navigatorShape === "open") || navigatorPosition !== "is-aside") && (
-            <CategoryFilter categories={categories} filterCategory={this.categoryFilterOnClick} />
+          {((isWideScreen && navigatorShape === 'open') ||
+            navigatorPosition !== 'is-aside') && (
+            <CategoryFilter
+              categories={categories}
+              filterCategory={this.categoryFilterOnClick}
+            />
           )}
           <IconButton
             aria-label="Search"
@@ -146,12 +156,12 @@ class ActionsBar extends React.Component {
             to="/search/"
             title="Search"
             className={classes.button}
-          >
-            <SearchIcon className={classes.button} />
-          </IconButton>
+          />
         </div>
         <div className={classes.group}>
-          {navigatorPosition === "is-aside" && <FontSetter increaseFont={this.fontSetterOnClick} />}
+          {navigatorPosition === 'is-aside' && (
+            <FontSetter increaseFont={this.fontSetterOnClick} />
+          )}
           {screenfull.enabled && (
             <IconButton
               aria-label="Fullscreen"
@@ -159,15 +169,23 @@ class ActionsBar extends React.Component {
               title="Fullscreen mode"
               className={classes.button}
             >
-              {this.state.fullscreen ? <FullscreenExitIcon /> : <FullscreenIcon />}
+              {this.state.fullscreen ? (
+                <FullscreenExitIcon />
+              ) : (
+                <FullscreenIcon />
+              )}
             </IconButton>
           )}
-          <IconButton aria-label="Back to top" onClick={this.arrowUpOnClick} title="Scroll to top">
+          <IconButton
+            aria-label="Back to top"
+            onClick={this.arrowUpOnClick}
+            title="Scroll to top"
+          >
             <ArrowUpwardIcon className={classes.button} />
           </IconButton>
         </div>
       </div>
-    );
+    )
   }
 }
 
@@ -180,27 +198,27 @@ ActionsBar.propTypes = {
   setFontSizeIncrease: PropTypes.func.isRequired,
   categories: PropTypes.array.isRequired,
   setCategoryFilter: PropTypes.func.isRequired,
-  categoryFilter: PropTypes.string.isRequired
-};
+  categoryFilter: PropTypes.string.isRequired,
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {
     navigatorPosition: state.navigatorPosition,
     navigatorShape: state.navigatorShape,
     isWideScreen: state.isWideScreen,
-    categoryFilter: state.categoryFilter
-  };
-};
+    categoryFilter: state.categoryFilter,
+  }
+}
 
 const mapDispatchToProps = {
   setNavigatorPosition,
   setNavigatorShape,
   setScrollToTop,
   setFontSizeIncrease,
-  setCategoryFilter
-};
+  setCategoryFilter,
+}
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(injectSheet(styles)(ActionsBar));
+)(injectSheet(styles)(ActionsBar))
